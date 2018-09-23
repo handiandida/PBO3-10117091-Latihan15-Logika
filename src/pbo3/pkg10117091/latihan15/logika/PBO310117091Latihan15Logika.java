24cb22b7473f7767471261352e7a754e14db36a2
Dida Handian <handiandida@gmail.com>
2018-09-23 20:01:26 +0700

    Logika

---
+++ src/pbo3/pkg10117091/latihan15/logika/PBO310117091Latihan15Logika.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117091.latihan15.logika;
+
+/* @author
+* NAMA  : DIDA HANDIAN
+* KELAS : PBO3
+* NIM   : 10117091
+* Deskripsi Program : Program ini berisi fungsi Logika Boolean ( True & False )
+*/
+
+public class PBO310117091Latihan15Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b) = " + !(a && b));
+        
+    }
+    
+}
